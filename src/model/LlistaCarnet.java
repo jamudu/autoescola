3d70279e1065285a596cfe84afb83f6852eacf2d
jamudu <olgha2002@gmail.com>
2016-06-17 12:52:57 +0200

    creades clsses llistes Alumne, Carnet, Matricula, Practica, Professor

---
+++ src/model/LlistaCarnet.java
@@ -0,0 +1,45 @@
+//
+
+package model;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.ArrayList;
+import org.jdesktop.observablecollections.ObservableCollections;
+import org.jdesktop.observablecollections.ObservableList;
+
+/**
+ *
+ * @author usu21
+ */
+public class LlistaCarnet {
+    
+    private ObservableList<Carnet> lista;
+
+    public LlistaCarnet() {
+        lista=ObservableCollections.observableList(new ArrayList<Carnet>());
+    }
+
+    public static final String PROP_LISTA = "lista";
+
+    public ObservableList<Carnet> getLista() {
+        return lista;
+    }
+
+    public void setLista(ObservableList<Carnet> lista) {
+        ObservableList<Carnet> oldLista = this.lista;
+        this.lista = lista;
+        propertyChangeSupport.firePropertyChange(PROP_LISTA, oldLista, lista);
+    }
+
+    private transient final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.removePropertyChangeListener(listener);
+    }
+
+}
